378be17766497367ffd68d249391d47b0b635ade
Gia Jennings <gjennings.co@gmail.com>
2021-11-09 03:37:36 -0500

    Remove frontend

--- frontend/src/reducers/artsReducer.js
+++
@@ -1,8 +0,0 @@
-export const artsReducer = (state = [], action) => {
-    switch(action.type){
-        case 'FETCH_ARTS':
-            return action.payload
-        default:
-            return state
-    }
-}
--- frontend/src/reducers/galleriesReducer.js
+++
@@ -1,8 +0,0 @@
-export const galleriesReducer = (state = [], action) => {
-    switch(action.type){
-        case 'FETCH_GALLERIES':
-            return action.payload
-        default:
-            return state
-    }
-}
