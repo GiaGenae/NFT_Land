e994efcd88ec1efdeb52af417356965839cc2f6e
Gia Jennings <gjennings.co@gmail.com>
2021-11-09 02:21:29 -0500

    Install redux packages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+
 
 ReactDOM.render(
   <React.StrictMode>
