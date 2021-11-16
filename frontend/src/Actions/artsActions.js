ba1fbfaed712d58470fb122802d151bc903e6849
Gia Jennings <gjennings.co@gmail.com>
2021-11-16 18:45:18 -0500

    Build out actions for arts object

---
+++ frontend/src/Actions/artsActions.js
@@ -0,0 +1,95 @@
+import {
+    IS_LOADING_ARTS,
+    GET_ARTS_SUCCESS,
+    GET_ARTS_FAILED,
+    DELETE_ART
+} from './actionTypes';
+
+ const getArtsByGalleryId = (galleryId) => {
+    return (dispatch) => {
+        dispatch({ type: IS_LOADING_ARTS });
+
+        fetch(`http://localhost:4000/galleries/${galleryId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response has an issue')
+                }
+                return response.json()
+            })
+            .then((data) => dispatch({
+                type: GET_ARTS_SUCCESS,
+                payload: {
+                    data: data.arts
+                }
+            }))
+            .catch(e => dispatch({ 
+                type: GET_ARTS_FAILED, 
+                payload: { error: e } 
+            }));
+    }
+}
+const getAllArts = () => {
+    return (dispatch) => {
+        dispatch({ type: IS_LOADING_ARTS });
+
+        fetch(`http://localhost:4000/arts`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response has an issue')
+                }
+                return response.json()
+            })
+            .then((data) => dispatch({
+                type: GET_ARTS_SUCCESS,
+                payload: {
+                    data
+                }
+            }))
+            .catch(e => dispatch({ 
+                type: GET_ARTS_FAILED, 
+                payload: { error: e } 
+            }));
+    }
+}
+
+const deleteArt = (artid) => {
+    const configObj = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }}
+
+   fetch(`http://localhost:4000/arts/${artid}`, configObj)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response has an issue')
+            }
+        })
+}
+
+export const addArt = (userInput) => {
+    let _data = {
+        "name": userInput.name,
+        "price": userInput.price,
+        "description": userInput.description,
+        "gallery_id": userInput.gallery,
+    }
+    const configObj = {
+      method: 'POST',
+      headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+      },
+      body: JSON.stringify(_data)
+  }
+  
+  fetch(`http://localhost:4000/arts`, configObj)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response has an issue')
+                }
+            })
+}
+
+export{getArtsByGalleryId,getAllArts, deleteArt}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Menu from './Components/Menu';
 import GalleriesList from './Components/GalleriesList';
-// import ArtsList from './Components/ArtsList';
+import ArtsList from './Components/ArtsList';
 import { Route,Routes } from 'react-router-dom';
 import Home from './Components/Home';
 
@@ -14,6 +14,8 @@ class App extends Component {
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='galleries' element={<GalleriesList />} />
+          <Route path='gallery/:galleryId' element={<ArtsList />} />
+          <Route path='arts' element={<ShowAllArts />} />
         </Routes>
       </div>
     )
