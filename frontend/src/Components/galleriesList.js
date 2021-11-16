f82cfba55fae780d0efce7b62240cab844cef4cd
Gia Jennings <gjennings.co@gmail.com>
2021-11-16 14:20:17 -0500

    Create galleries list map each gallery and dispatch

--- frontend/src/Components/galleriesList.js
+++ frontend/src/Components/galleriesList.js
@@ -3,3 +3,47 @@ import { fetchGalleries } from '../Actions/galleriesActions';
 import { connect } from 'react-redux';
 import './GalleriesList.css';
 import Gallery from './Gallery';
+
+class GalleriesList extends React.Component {
+    componentDidMount() {
+        this.props.loadGalleries();
+    }
+
+    displayGalleries() {
+        return (
+            this.props.galleries.map(gallery =>
+                <Gallery key={gallery.id} gallery={gallery} />
+            )
+        )
+    }
+    render() {
+        const { isLoading, error } = this.props;
+        return (
+            <div className="row">
+                {isLoading && <h1>Loading...</h1>}
+                {error && (
+                    <div className="alert alert-danger" role="alert">
+                        Sorry! We've encountered an error while fetching galleries.
+                    </div>
+                )}
+                {this.displayGalleries()}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        isLoading: state.galleriesReducer.isLoading,
+        galleries: state.galleriesReducer.galleries,
+        error: state.galleriesReducer.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        loadGalleries: () => dispatch(fetchGalleries())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(GalleriesList);
