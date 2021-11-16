a01b428402b4ec3af0e91e9a0120870230cb0108
Gia Jennings <gjennings.co@gmail.com>
2021-11-16 13:47:51 -0500

    Write fetch request for gallery data

---
+++ frontend/src/Actions/galleriesActions.js
@@ -0,0 +1,28 @@
+import {
+    IS_LOADING,
+    GET_GALLERIES_SUCCESS,
+    GET_GALLERIES_FAILED,
+} from './actionTypes.js';
+
+export const fetchGalleries = () => {
+    return (dispatch) => {
+        dispatch({ type: IS_LOADING });
+        fetch(`http:localhost:4000/galleries`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('There is an issue with the network response.')
+            }
+            return response.json()
+        })
+        .then((data) => dispatch({
+            type: GET_GALLERIES_SUCCESS,
+            payload: {
+                data
+            }
+        }))
+        .catch(e => dispatch({
+            type: GET_GALLERIES_FAILED,
+            payload: { error: e }
+        }));
+    }
+}
